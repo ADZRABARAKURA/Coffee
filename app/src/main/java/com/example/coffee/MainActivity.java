9c18fa86e83bb463aa11f52cedb0d66dbeae6926
ADZRABARAKURA <alekseyusov51@gmail.com>
2021-05-17 01:23:23 +0500

    update

--- app/src/main/java/com/example/coffee/MainActivity.java
+++ app/src/main/java/com/example/coffee/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.coffee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView CoffeCraft;
+    private Button theory;
+    private Button practice;
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CoffeCraft = findViewById(R.id.CoffeCraft);
     }
 }
